b2cd696c9631a35bab1f9682f0291a26b029e470
i-am-rk <dev.ravikiran.rk@gmail.com>
2020-04-12 10:37:15 +0530

    invoice file added

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,14 +1,17 @@
-const Product = require('../models/product');
-const Order = require('../models/order');
+const fs = require("fs");
+const path = require("path");
+
+const Product = require("../models/product");
+const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then(products => {
       console.log(products);
-      res.render('shop/product-list', {
+      res.render("shop/product-list", {
         prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
+        pageTitle: "All Products",
+        path: "/products"
       });
     })
     .catch(err => {
@@ -20,10 +23,10 @@ exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
     .then(product => {
-      res.render('shop/product-detail', {
+      res.render("shop/product-detail", {
         product: product,
         pageTitle: product.title,
-        path: '/products'
+        path: "/products"
       });
     })
     .catch(err => console.log(err));
@@ -32,10 +35,10 @@ exports.getProduct = (req, res, next) => {
 exports.getIndex = (req, res, next) => {
   Product.find()
     .then(products => {
-      res.render('shop/index', {
+      res.render("shop/index", {
         prods: products,
-        pageTitle: 'Shop',
-        path: '/'
+        pageTitle: "Shop",
+        path: "/"
       });
     })
     .catch(err => {
@@ -45,13 +48,13 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .populate('cart.items.productId')
+    .populate("cart.items.productId")
     .execPopulate()
     .then(user => {
       const products = user.cart.items;
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
         products: products
       });
     })
@@ -65,8 +68,8 @@ exports.postCart = (req, res, next) => {
       return req.user.addToCart(product);
     })
     .then(result => {
-      console.log(result);
-      res.redirect('/cart');
+      // console.log(result);
+      res.redirect("/cart");
     });
 };
 
@@ -75,14 +78,14 @@ exports.postCartDeleteProduct = (req, res, next) => {
   req.user
     .removeFromCart(prodId)
     .then(result => {
-      res.redirect('/cart');
+      res.redirect("/cart");
     })
     .catch(err => console.log(err));
 };
 
 exports.postOrder = (req, res, next) => {
   req.user
-    .populate('cart.items.productId')
+    .populate("cart.items.productId")
     .execPopulate()
     .then(user => {
       const products = user.cart.items.map(i => {
@@ -101,19 +104,33 @@ exports.postOrder = (req, res, next) => {
       return req.user.clearCart();
     })
     .then(() => {
-      res.redirect('/orders');
+      res.redirect("/orders");
     })
     .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  Order.find({ 'user.userId': req.user._id })
+  Order.find({ "user.userId": req.user._id })
     .then(orders => {
-      res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders',
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
         orders: orders
       });
     })
     .catch(err => console.log(err));
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName = "invoice-" + orderId + ".pdf";
+  const invoicePath = path.join("data", "invoices", invoiceName);
+
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      console.log(err)
+      return next();
+    }
+    res.send(data);
+  });
 };
